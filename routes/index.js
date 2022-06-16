ac62a601bd10513ae6cab5694fc2b69daa07ba8e
AC-Galen <lottaie7359882@gmail.com>
2022-06-17 00:11:04 +0800

    feat: add user modules

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,12 @@ const express = require("express")
 const router = express.Router()
 const index = require("./modules/index")
 const restaurants = require("./modules/restaurants")
+const users = require('./modules/users')
 
 
 router.use('/', index)
 router.use('/search', index)
 router.use('/restaurants', restaurants)
+router.use('/users', users)
 
 module.exports = router
