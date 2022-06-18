bf58d09b88fb3fad4d4a5b4f38e8584e80658ca8
AC-Galen <lottaie7359882@gmail.com>
2022-06-18 15:41:43 +0800

    feat add relations between User and Todo models

--- routes/modules/restaurants.js
+++ routes/modules/restaurants.js
@@ -7,38 +7,52 @@ router.get('/new', (req, res) => {
   res.render('new')
 })
 
-router.get("/:restaurantId", (req, res) => {
-  const { restaurantId } = req.params
-  Restaurant.findById(restaurantId)
+router.get("/:id", (req, res) => {
+  const userId = req.user._id
+  const _id = req.params.id
+  Restaurant.findOne({ _id, userId })
     .lean()
     .then(restaurantData => res.render("show", { restaurantData }))
     .catch(err => console.log(err))
 })
 
-router.post("/restaurants", (req, res) => {
-  Restaurant.create(req.body)
-    .then(() => res.redirect("/"))
-    .catch(err => console.log(err))
+router.post('/', (req, res) => {
+  const restaurantData = req.body 
+  restaurantData.userId = req.user._id 
+  Restaurant.create(restaurantData)
+    .then(() => res.redirect('/'))
+    .catch(error => console.error(error))
 })
 
-router.get("/:restaurantId/edit", (req, res) => {
-  const { restaurantId } = req.params
-  Restaurant.findById(restaurantId)
+router.get("/:id/edit", (req, res) => {
+  const userId = req.user._id
+  const _id = req.params.id
+  Restaurant.findOne({ _id, userId })
     .lean()
     .then(restaurantData => res.render("edit", { restaurantData }))
     .catch(err => console.log(err))
 })
 
-router.put("/:restaurantId", (req, res) => {
-  const { restaurantId } = req.params
-  Restaurant.findByIdAndUpdate(restaurantId, req.body)
-    .then(() => res.redirect(`/restaurants/${restaurantId}`))
+router.put("/:id", (req, res) => {
+  const userId = req.user._id
+  const _id = req.params.id
+  const result = req.body
+  Restaurant.findOne({ _id, userId })
+    .then(restaurant => {
+      for (const key of Object.keys(result)) {
+        restaurant[key] = result[key]
+      }
+      return restaurant.save()
+    })
+    .then(() => res.redirect(`/restaurants/${_id}`))
     .catch(err => console.log(err))
 })
 
-router.delete("/:restaurantId", (req, res) => {
-  const { restaurantId } = req.params
-  Restaurant.findByIdAndDelete(restaurantId)
+router.delete("/:id", (req, res) => {
+  const userId = req.user._id
+  const _id = req.params.id
+  Restaurant.findOne({ _id, userId})
+    .then(restaurant => restaurant.remove())
     .then(() => res.redirect("/"))
     .catch(err => console.log(err))
 })
